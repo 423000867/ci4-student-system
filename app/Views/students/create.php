b9d03cf6cc6dc44bfb348c2e4bdb7aa5868b99a7
purplebreadwithcoconut <mesinabiancanicole@gmail.com>
2026-05-04 22:10:32 +0800

    Feature: CRUD Student Management (Mesina)

---
+++ app/Views/students/create.php
@@ -0,0 +1,14 @@
+<h2>Add Student</h2>
+
+<form action="/students/store" method="post">
+    <label>Name:</label><br>
+    <input type="text" name="name"><br>
+
+    <label>Email:</label><br>
+    <input type="email" name="email"><br>
+
+    <label>Course:</label><br>
+    <input type="text" name="course"><br><br>
+
+    <button type="submit">Save</button>
+</form>
---
+++ app/Views/students/index.php
@@ -0,0 +1,25 @@
+<h2>Student List</h2>
+
+<a href="/students/create">Add Student</a>
+
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Course</th>
+        <th>Action</th>
+    </tr>
+
+    <?php foreach ($students as $student): ?>
+    <tr>
+        <td><?= $student['id'] ?></td>
+        <td><?= $student['name'] ?></td>
+        <td><?= $student['email'] ?></td>
+        <td><?= $student['course'] ?></td>
+        <td>
+            <a href="/students/delete/<?= $student['id'] ?>">Delete</a>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
